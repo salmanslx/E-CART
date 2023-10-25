538c0a04b031a9b6aa6b0c7fef13334adb61bedd
salmanslx <salmanulfaris712@gmail.com>
2023-10-25 11:30:51 +0530

    V2

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Home from './Pages/Home';
+import Cart from './Pages/Cart';
+import Whishlist from './Pages/Wishlist';
+import Header from './Components/Header';
+import Footer from './Components/Footer';
 
 function App() {
   return (
-    <div className="App">
-      <h1>E-cart</h1>
-    </div>
+    <>
+    <Header/>
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/cart' element={<Cart/>} />
+        <Route path='/wishlist' element={<Whishlist/>} />
+      </Routes>
+      <Footer/>
+    </>
   );
 }
 
